8b8dad568639361196d3d2f076f5864a459229fc
Y ADHARSH KUMAR <91778905+adharshkumar07@users.noreply.github.com>
2026-02-23 11:19:41 +0530

    Update provider.tf

--- Terraform/04_variables/provider.tf
+++ Terraform/04_variables/provider.tf
@@ -4,15 +4,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.9.0"
+      version = "=4.61.0"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-
-  subscription_id = "xxxxxx-xxxxxx-xxxxxxxx-xxxxxxx"
+  subscription_id = "bad976a3-efdf-4d47-b37c-2eaf2b10b6ec"
 }
